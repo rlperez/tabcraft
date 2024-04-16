9e3f108e798360e6d773f627e411d79029e2d50b
Rigoberto L. Perez <rlperez@kablamo.me>
2024-04-16 00:03:39 -0400

    fix: 🐛 fill in some of the standard return type interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,11 @@ declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			msg: string;
+			isError: boolean;
+			data: Record<string, unknown>;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
