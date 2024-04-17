a6e4baa42fd29ea2603ac738886354a03b6c02eb
Rigoberto L. Perez <rlperez@kablamo.me>
2024-04-17 16:52:19 -0400

    refactor: 💡 remove idiotic interface defnition for tailwind con

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,27 +11,6 @@ declare global {
 		}
 		// interface PageState {}
 		// interface Platform {}
-
-		interface TailwindConfig {
-			content: string[];
-			theme: {
-				container: {
-					padding: string;
-				};
-			};
-			daisyui: {
-				themes: string[] | boolean;
-				theme: string;
-				darkTheme: string;
-				lightTheme: string;
-				base: boolean;
-				styled: boolean;
-				utils: boolean;
-				prefix: string;
-				logs: boolean;
-				themeRoot: string;
-			};
-		}
 	}
 }
 
