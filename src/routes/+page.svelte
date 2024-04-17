4d3dd639a414cbd0c56b448f98188304bbb37e61
Rigoberto L. Perez <rlperez@kablamo.me>
2024-04-17 10:44:44 -0400

    feat: 🎸 add menu model to show theme options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,26 @@
 	<div class="navbar bg-base-100">
 		<div class="flex-1"></div>
 		<div class="flex-none space-x-2 my-8">
-			<button class="ghost btn btn-sm btn-circle"><i class="fa-solid fa-palette"></i></button>
-			<button class="ghost btn btn-sm btn-circle"><i class="fa-solid fa-cog"></i></button>
+			<details>
+				<summary>
+					<span class="ghost btn btn-sm btn-circle"><i class="fa-solid fa-palette"></i></span>
+				</summary>
+				<div class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+					<div
+						class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-gray-50/5 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5"
+					>
+						<div class="p-4">
+							<div class="flex flex-col w-full">
+								<div class="divider divider-start text-lg font-bold">Themes</div>
+							</div>
+						</div>
+						<div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50"></div>
+					</div>
+				</div>
+			</details>
+			<button class="ghost btn btn-sm btn-circle">
+				<i class="fa-solid fa-cog"></i>
+			</button>
 		</div>
 	</div>
 </header>
